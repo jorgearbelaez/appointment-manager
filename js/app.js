c500d8850649ca8e22f9035352adecb347c7de72
Jorge Arbelaez Carmona <you@example.com>
2022-09-23 10:34:35 -0500

    se completa la aplicacion habilitando la eliminacion y edicion de citas

--- js/app.js
+++ js/app.js
@@ -10,6 +10,8 @@ const formulario = document.querySelector("#nueva-cita");
 
 const contenedorCitas = document.querySelector("#citas");
 
+let editando;
+
 //clases
 class Citas {
   constructor() {
@@ -20,6 +22,15 @@ class Citas {
     this.citas = [...this.citas, cita];
 
     console.log(this.citas);
+  }
+  eliminarCita(id) {
+    this.citas = this.citas.filter((cita) => cita.id !== id);
+    console.log(this.citas);
+  }
+  editarCita(citaEditada) {
+    this.citas = this.citas.map((cita) =>
+      cita.id === citaEditada.id ? citaEditada : cita
+    );
   }
 }
 class Interfaz {
@@ -84,17 +95,35 @@ class Interfaz {
       sintomasParrafo.innerHTML = `
       <span class="font-weight-bolder">Sintomas: </span> ${sintomas}`;
 
+      //boton para eliminar las citas
+
+      const btnEliminar = document.createElement("button");
+      btnEliminar.classList.add("btn", "btn-danger", "mr-2");
+      btnEliminar.innerHTML =
+        'Eliminar <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>';
+      btnEliminar.onclick = () => eliminarCita(id);
+
+      //editar cita
+      const btnEditar = document.createElement("button");
+      btnEditar.classList.add("btn", "btn-info", "mr-2");
+      btnEditar.innerHTML =
+        'Editar <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path></svg>';
+      btnEditar.onclick = () => cargarEdicion(cita);
+
       divCita.appendChild(mascotaParrafo);
       divCita.appendChild(propietarioParrafo);
       divCita.appendChild(telefonoParrafo);
       divCita.appendChild(fechaParrafo);
       divCita.appendChild(horaParrafo);
       divCita.appendChild(sintomasParrafo);
+      divCita.appendChild(btnEliminar);
+      divCita.appendChild(btnEditar);
 
       //agregar al html
       contenedorCitas.appendChild(divCita);
     });
   }
+
   limpiarHTML() {
     while (contenedorCitas.firstChild)
       contenedorCitas.removeChild(contenedorCitas.firstChild);
@@ -156,15 +185,34 @@ function nuevaCita(e) {
     return;
   }
 
-  //pasa validacion generamos id
+  if (editando) {
+    //reingresando la cita con los cambios
+    administradorCitas.editarCita({ ...citaObj });
 
-  citaObj.id = Date.now();
+    // mensaje de exito
+    interfaz.mensajeAlerta("Cambios guardados");
+    // cmabiar texto boton
+    formulario.querySelector('button[type ="submit"]').textContent =
+      "Crear Cita";
+    // cerrar modo edicion
+    editando = false;
+  } else {
+    //pasa validacion generamos id
 
-  // creando nueva cita
-  administradorCitas.agregarCita({ ...citaObj });
+    citaObj.id = Date.now();
+
+    // creando nueva cita
+    administradorCitas.agregarCita({ ...citaObj });
+
+    // mensaje de exito
+    interfaz.mensajeAlerta("Se agrego correctamente");
+  }
 
   //resetear formulario
   formulario.reset();
+
+  //reiniciarObjeto
+  reiniciarObjeto();
 
   //mostrar el html de las citas
   interfaz.mostrarCitas(administradorCitas);
@@ -177,4 +225,42 @@ function reiniciarObjeto() {
   citaObj.fecha = "";
   citaObj.hora = "";
   citaObj.sintomas = "";
+}
+function eliminarCita(id) {
+  //mandar a llamar el metodo que me eliminara la cita
+  administradorCitas.eliminarCita(id);
+
+  // mostrar mensaje de exito al borrar la cita
+  interfaz.mensajeAlerta("Se ha borrado exitosamente la cita");
+
+  //actualizar citas en el html
+  interfaz.mostrarCitas(administradorCitas);
+}
+
+// cargar los datos para la edicion
+function cargarEdicion(cita) {
+  const { mascota, propietario, telefono, fecha, hora, sintomas, id } = cita;
+
+  // llenar los imputs
+  mascotaInput.value = mascota;
+  propietarioInput.value = propietario;
+  telefonoInput.value = telefono;
+  fechaInput.value = fecha;
+  horaInput.value = hora;
+  sintomasInput.value = sintomas;
+
+  // llenar el objeto global
+  citaObj.mascota = mascota;
+  citaObj.propietario = propietario;
+  citaObj.telefono = telefono;
+  citaObj.fecha = fecha;
+  citaObj.hora = hora;
+  citaObj.sintomas = sintomas;
+  citaObj.id = id;
+
+  //cambiar texto button
+  formulario.querySelector('button[type ="submit"]').textContent =
+    "Guardar Cambios";
+
+  editando = true;
 }
